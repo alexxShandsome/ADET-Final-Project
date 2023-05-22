ad76ca8754d13b26cda174ea3207faa537652e19
root <algilbertgomez@gmail.com>
2023-05-22 21:55:13 +0800

    initial code

--- database_connection.php
+++ database_connection.php
@@ -3,4 +3,7 @@
 	// new database connection for memesite
 	$pdo = new PDO('mysql:host=localhost;port=3306;dbname=adet_final_project', 'root', '');
 
+	// set error
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 ?>
