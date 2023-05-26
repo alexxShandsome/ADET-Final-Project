51ea51103e26107f535712f6f19a3e58f4bbd99c
root <algilbertgomez@gmail.com>
2023-05-26 08:46:33 +0800

    landing: blur cards and add github icon

--- landing.php
+++ landing.php
@@ -34,9 +34,9 @@
 	</nav>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-6">
+			<div class="col-md-7">
 				<div class="row">
-					<div class="col-md-12 rounded" style="background-color: rgba(108, 117, 125, 0.25);">
+					<div class="col-md-12 rounded" style="background-color: rgba(108, 117, 125, 0.25); backdrop-filter: blur(10px)">
 						<div class="row">
 							<h2>Hash App</h2>
 							<p>
@@ -51,22 +51,22 @@
 					</div>
 				</div>
 				<div class="row mt-2">
-					<div class="col-md-12 rounded" style="background-color: rgba(108, 117, 125, 0.25);">
+					<div class="col-md-12 rounded" style="background-color: rgba(108, 117, 125, 0.25); backdrop-filter: blur(10px)">
 						<div class="row">
 							<h2>Encryption and Decryption App</h2>
 							<p>This Python app allows you to encrypt and decrypt your sensitive data using various encryption algorithms.</p>
 						</div>
 						<div class="row">
-						<a class="btn btn-success" href="./downloadables/Encryption.zip">
+							<a class="btn btn-success" href="./downloadables/Encryption.zip">
 								Download Encryptor and Decryptor
 							</a>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="col-md-5 ms-3">
+			<div class="col ms-3">
 				<div class="row me-2">
-					<div class="col-md-12 rounded" style="background-color: rgba(108, 117, 125, 0.25);">
+					<div class="col-md-12 rounded" style="background-color: rgba(108, 117, 125, 0.25); backdrop-filter: blur(10px)">
 						<h3>Members</h3>
 						<ul>
 							<li>Algilbert Gomez</li>
@@ -77,21 +77,16 @@
 							<li>Ken Daryl Perez</li>
 						</ul>
 					</div>
-				</div>
-				<div class="row mt-2">
-					<div class="col-md-12 rounded">
-						<div class="d-flex flex-row-reverse bd-highlight">
-							<div class="p-2 bd-highlight">
-								<a class="btn btn-success" href="https://github.com/alexxShandsome/ADET-Final-Project" target="_blank">
-									↓ Project Repository
-								</a>
-							</div>
-						</div>
-					</div>
+					<a class="btn btn-success" href="https://github.com/alexxShandsome/ADET-Final-Project" target="_blank">
+						<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
+						  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+						</svg>
+						Project Repository
+					</a>
 				</div>
 			</div>
 		</div>
-</div>
+	</div>
 
 	<script src="./bootstrap/js/bootstrap.min.js"></script>
 </body>
